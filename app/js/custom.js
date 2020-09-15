4e23318cb2b5bb3ba4a24e006e56ca4ea015d7b7
EvgeniyStepura <jack_sem@rambler.ru>
2020-09-15 09:50:55 +0300

    start

---
+++ app/js/custom.js
@@ -0,0 +1,103 @@
+$(window).on('load',function(){
+  $(".js-bg").each(function () {
+      $(this).css('background-image', 'url(' + $(this).data("preload") + ')');
+  });
+  $(".js-img").each(function () {
+      $(this).attr('src', $(this).data("src"));
+  });
+
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+		$('body').addClass('ios');
+        
+	} else{
+		$('body').addClass('web');
+        
+	};  
+	
+  setTimeout(function(){
+    $('body').removeClass('loaded'); 
+  },700);
+
+
+  setTimeout(function(){
+    $('.butWrapper .tooltip').addClass('show'); 
+  },9000);
+
+});
+/* viewport width */
+function viewport(){
+	var e = window, 
+		a = 'inner';
+	if ( !( 'innerWidth' in window ) )
+	{
+		a = 'client';
+		e = document.documentElement || document.body;
+	}
+	return { width : e[ a+'Width' ] , height : e[ a+'Height' ] }
+};
+/* viewport width */
+
+
+$(function(){
+	/* placeholder*/	   
+	$('.form-control').each(function(){
+ 		var placeholder = $(this).attr('placeholder');
+ 		$(this).focus(function(){ $(this).attr('placeholder', '');});
+ 		$(this).focusout(function(){			 
+ 			$(this).attr('placeholder', placeholder);  			
+ 		});
+ 	});  
+	/* placeholder*/ 
+
+  $(".header .js-img").each(function () {
+      $(this).attr('src', $(this).data("src"));
+  });
+
+
+  //
+  if ($('.fancybox').length) {
+    $('.fancybox').fancybox({closeExisting: true});
+  }
+
+
+});
+
+//
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+});
+
+var handler = function(){
+	  
+  var height_footer = $('.footer').height();	
+  var height_header = $('.header').height();		
+
+  var viewport_wid = viewport().width;
+  var viewport_height = viewport().height;
+
+
+  if (viewport_wid <= 991) {
+    
+  }
+  else if (viewport_wid > 991) {
+    
+  }
+
+  function showDiv() {
+    if ( $(window).scrollTop() > 0 && $('.header').data('positioned') == 'false') {
+      $(".header").data('positioned', 'true');
+      $(".header").addClass('fix');
+    }else if ( $(window).scrollTop() <= 0 && $('.header').data('positioned') == 'true') {
+      $(".header").fadeIn(0, function() {
+        $(".header").removeClass('fix');
+      }).data('positioned', 'false');
+    }
+  }
+  $(window).scroll(showDiv);
+  $('.header').data('positioned', 'false');
+  
+	
+}
+$(window).bind('load', handler);
+$(window).bind('resize', handler);
